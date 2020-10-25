ee466b9c783b27dfbbef36384d1c71746e55c904
Maxwell Beck <max@ryt.one>
2020-10-24 19:05:35 -0500

    Reformat JSON export to include a bit more information
    Should be usable now for automatic audio tagging

--- www_admin/plugins/json_export/index.php
+++ www_admin/plugins/json_export/index.php
@@ -4,14 +4,19 @@ header('Content-Type: text/json');
 
 $data = array();
 
-$compos = SQLLib::selectRows("select id,name from compos order by start");
+$compos = SQLLib::selectRows("select id,name,dirname from compos order by start");
 foreach ($compos as $compo) {
     $entries = SQLLib::selectRows(sprintf_esc(
         "select title,author,comment,orgacomment,filename from compoentries where compoid = %d order by playingorder",
         $compo->id
     ));
 
-    $data[$compo->name] = $entries;
+    $compodata = array();
+    $compodata["directory_name"] = $compo->dirname;
+    $compodata["display_name"] = $compo->name;
+    $compodata["entries"] = $entries;
+
+    array_push($data, $compodata);
 }
 
 echo json_encode($data);
